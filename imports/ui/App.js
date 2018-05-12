a68333546455085825ae52574c3c86cf0da8a4f2
juanSebastian <js.barragan796@uniandes.edu.co>
2018-05-12 13:42:48 -0500

    Front ! :sun_with_face:

---
+++ imports/ui/App.js
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withTracker } from "meteor/react-meteor-data";
+import { Meteor } from "meteor/meteor";
+import { Collection } from "../api/collection.js";
+import { User } from "../api/collection.js";
+import { UserTweets } from "../api/collection.js";
+import { Following } from "../api/collection.js";
+import Visualizacion from "./Visualizacion.js";
+import { Jumbotron,
+  Container,
+  Form,
+  FormGroup,
+  InputGroup,
+  Input,
+  Label,
+  InputGroupAddon,
+  Button
+} from "reactstrap";
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      queryInput: ""
+    };
+    this.handleChangeQuery = this.handleChangeQuery.bind(this);
+    this.makeQuery = this.makeQuery.bind(this);
+  }
+
+  handleChangeQuery (event) {
+    this.setState({ queryInput: event.target.value });
+  }
+  makeQuery (event) {
+    event.preventDefault();
+    const query = this.state.queryInput;
+    if (query !== "") {
+      console.log("Query: " + query);
+      Meteor.call("twitter.get.user.data", query, (error, result) => {
+        if (error) {
+          // handle the error
+        } else {
+          console.log(result);
+        }
+      });
+    }
+  }
+  searchBar () {
+    return (
+      <Form className="new-task" onSubmit={this.makeQuery} >
+        <FormGroup>
+          <Label for="query">Insert Twitter account: </Label>
+          <InputGroup>
+            <InputGroupAddon addonType="prepend">@</InputGroupAddon>
+            <Input
+              id="query"
+              type="text"
+              value = {this.state.queryInput}
+              onChange={this.handleChangeQuery}
+              placeholder="account"
+            />
+            <InputGroupAddon addonType="append">
+              <Button color="secondary">Enviar</Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </FormGroup>
+      </Form>);
+  }
+
+  render () {
+    console.log("render!");
+    return (
+      <div>
+        <Jumbotron fluid>
+          <Container fluid>
+            <h1 className="display-3">Fluid jumbotron</h1>
+            <p className="lead">This is a modified jumbotron that occupies the
+            entire horizontal space of its parent.</p>
+          </Container>
+        </Jumbotron>
+        <div className="contenido">
+          {this.searchBar()}
+        </div>
+        <Visualizacion collection={this.props.collection} />
+      </div>
+    );
+  }
+}
+//prop types for App
+App.propTypes = {
+  user: PropTypes.array.isRequired,
+  collection: PropTypes.array.isRequired,
+  tweets: PropTypes.array.isRequired,
+  following: PropTypes.array.isRequired
+};
+export default withTracker(() => {
+  Meteor.subscribe("collection");
+  Meteor.subscribe("user");
+  Meteor.subscribe("usertweets");
+  Meteor.subscribe("following");
+  return {
+    collection: Collection.find({}).fetch(),
+    user: User.find({}).fetch(),
+    tweets: UserTweets.find({}).fetch(),
+    following: Following.find({}).fetch()
+  };
+})(App);
---
+++ imports/ui/Visualizacion.js
@@ -0,0 +1,138 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import * as d3 from "d3";
+import { Row, Col } from "reactstrap";
+
+export default class Visualizacion extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount () {
+    console.log("entrooooo");
+    const graph = {
+      nodes: [
+        { name: "hola", age: 35 },
+        { name: "hola1", age: 35 },
+        { name: "hola2", age: 33 },
+        { name: "hola3", age: 2 },
+        { name: "hola4", age: 25 },
+        { name: "hola5", age: 85 }
+      ],
+      links: [
+        { source: "hola", target: "hola1" },
+        { source: "hola", target: "hola2" },
+        { source: "hola", target: "hola3" },
+        { source: "hola", target: "hola4" },
+        { source: "hola", target: "hola5" },
+        { source: "hola5", target: "hola2" }
+      ]
+    };
+    var canvas = d3.select(this.canvas);
+    var width = canvas.attr("width");
+    var height = canvas.attr("height");
+    var ctx = canvas.node().getContext("2d");
+    var r = 10;
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
+    var simulation = d3.forceSimulation()
+      .force("x", d3.forceX(width / 2))
+      .force("y", d3.forceY(width / 2))
+      .force("collide", d3.forceCollide(r + 1))
+      .force("charge", d3.forceManyBody(r + 1).strength(-200))
+      .on("tick", update)
+      .force("link", d3.forceLink().id((d) => {return d.name;}));
+
+    // graph.nodes.forEach((d) => {
+    //   d.x = Math.random() * width;
+    //   d.y = Math.random() * height;
+    // });
+    simulation.nodes(graph.nodes);
+    simulation.force("link")
+      .links(graph.links);
+
+    canvas.call(d3.drag()
+      .container(canvas.node())
+      .subject(dragsubject)
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended));
+
+    function dragsubject () {
+      return simulation.find(d3.event.x, d3.event.y);
+    }
+
+    function update () {
+      ctx.clearRect(0, 0, width, height);
+
+      ctx.beginPath();
+      ctx.globalAlpha = 3;
+      ctx.strokeStyle = "#aaa";
+      graph.links.forEach(drawLink);
+      ctx.stroke();
+
+
+      ctx.globalAlpha = 1;
+      graph.nodes.forEach(drawNode);
+    }
+
+    function drawNode (d) {
+      ctx.beginPath();
+      ctx.fillStyle = color(d.party);
+      ctx.moveTo(d.x, d.y);
+      ctx.arc(d.x, d.y, r, 0, Math.PI * 2);
+      ctx.fill();
+    }
+    function drawLink (d) {
+      ctx.moveTo(d.source.x, d.source.y);
+      ctx.lineTo(d.target.x, d.target.y);
+    }
+
+    function dragstarted () {
+      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+      d3.event.subject.fx = d3.event.subject.x;
+      d3.event.subject.fy = d3.event.subject.y;
+    }
+
+    function dragged () {
+      d3.event.subject.fx = d3.event.x;
+      d3.event.subject.fy = d3.event.y;
+    }
+
+    function dragended () {
+      if (!d3.event.active) simulation.alphaTarget(0);
+      d3.event.subject.fx = null;
+      d3.event.subject.fy = null;
+    }
+    update();
+  }
+
+
+  render () {
+    return (
+      <div className="visualizacion">
+        <Row>
+          <Col sm="3" className="centered">
+            { }
+          </Col>
+          <Col sm="9" className="centered">
+            <canvas
+              width="400"
+              height="400"
+              ref = {(canvas) => {this.canvas = canvas; return this.canvas; }}>
+              vizualizacion de force
+            </canvas>
+          </Col>
+        </Row>
+
+      </div>
+    );
+  }
+}
+
+
+//Props del Home
+Visualizacion.propTypes = {
+  collection: PropTypes.array.isRequired
+};
